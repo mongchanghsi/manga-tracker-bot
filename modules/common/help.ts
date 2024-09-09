1725c1ceef141248ad944ce2537348eba8a37271
Eric Mong <m.changhsi@gmail.com>
2024-09-09 23:33:35 +0800

    Update help message

--- modules/common/help.ts
+++ modules/common/help.ts
@@ -2,10 +2,12 @@ import { Context, NarrowedContext } from "telegraf";
 import { MountMap } from "telegraf/typings/telegram-types";
 import * as tt from "telegraf/typings/telegram-types";
 
+const responseMessage = `Get started with /list /add and /remove\n\nStart tracking your favourite manga on their own site. Take note that this will not work accurately as different manga site handles their new chapter differently!`;
+
 export const HelpCommand = async (
   ctx: NarrowedContext<Context, MountMap["text"]>
 ) => {
-  ctx.reply("Help");
+  ctx.reply(responseMessage);
 };
 
 export const HelpAction = async (
@@ -15,5 +17,5 @@ export const HelpAction = async (
   >
 ) => {
   ctx.answerCbQuery();
-  ctx.reply("Help");
+  ctx.reply(responseMessage);
 };
