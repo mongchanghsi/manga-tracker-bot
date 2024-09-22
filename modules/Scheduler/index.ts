39f652e18fafdc5ea4a0e8dc59755ec2a56e5627
Eric Mong <m.changhsi@gmail.com>
2024-09-22 21:41:46 +0800

    Add logs for tracking new chapters

--- modules/Scheduler/index.ts
+++ modules/Scheduler/index.ts
@@ -9,9 +9,28 @@ import { checkIfUrlExist } from "../../utils/checker";
 const SCHEDUELD_TIME_2 = "00 00 00 * * *"; // Every day at 12am;
 const SCHEDUELD_TIME = "00 00 */6 * * *"; // Every 6 hours;
 
+const getCurrentTime = (): string => {
+  const _date = new Date();
+
+  const day = String(_date.getDate()).padStart(2, "0");
+  const month = String(_date.getMonth() + 1).padStart(2, "0"); // Months are 0-based
+  const year = _date.getFullYear();
+
+  let hours: any = _date.getHours();
+  const minutes = String(_date.getMinutes()).padStart(2, "0");
+  const ampm = hours >= 12 ? "PM" : "AM";
+
+  hours = hours % 12; // Convert to 12-hour format
+  hours = hours ? String(hours).padStart(2, "0") : "12"; // '0' should be '12'
+
+  return `${day}/${month}/${year} ${hours}:${minutes}${ampm}`;
+};
+
 const ScheduleUpdateBookmarks = async (
   bot: Telegraf<BookmarkSessionContext<Update>>
 ) => {
+  console.log(`Checking for new chapters @ ${getCurrentTime()}`);
+
   const users = await userDb.getAllUser();
   users.forEach(async (_user) => {
     const bookmarks = await listDb.getBookmarks(_user.telegramId);
