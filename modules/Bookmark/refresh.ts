a8d09f1c1f736dafc2c97646394757e5d4861068
Eric Mong <m.changhsi@gmail.com>
2025-03-28 00:09:24 +0800

    fix: update refresh to refresh all data instead

--- modules/Bookmark/refresh.ts
+++ modules/Bookmark/refresh.ts
@@ -1,20 +1,21 @@
 import { NarrowedContext, Types } from "telegraf";
 import { getUserIdFromCallback } from "../../utils/telegramHelper";
-import listDb from "../../database/List";
+import listDb, { PAGE_SIZE } from "../../database/List";
 import { BookmarkSessionContext } from "./session";
 import { Update } from "telegraf/types";
 import { checkIfUrlExist } from "../../utils/checker";
 import { COMMANDS } from "../../utils/command";
 import { Bookmark } from "../../utils/types";
+import { DEFAULT_GET_INLINE_KEYBOARD_COMMANDS } from "../common/commands";
 
-const getResponseStringBookmark = (bookmarks: Bookmark[]) => {
+const getResponseStringBookmark = (bookmarks: Bookmark[], page: number = 0) => {
   const _list = bookmarks
     .map(
       (bookmark) =>
         `${bookmark.id}. ${bookmark.name} - Chapter ${bookmark.latestChapter} - ${bookmark.url}`
     )
     .join(`\n`);
-  return `Here's the refreshed list\n\n${_list}`;
+  return `Here's the refreshed list - Page ${page + 1}\n\n${_list}`;
 };
 
 export const RefreshBookmarksAction = async (
@@ -24,7 +25,10 @@ export const RefreshBookmarksAction = async (
   >
 ) => {
   const userId = getUserIdFromCallback(ctx);
-  const bookmarks = await listDb.getBookmarks(userId);
+  const pageNumber = (
+    ctx as BookmarkSessionContext<Update> & { match: RegExpExecArray }
+  ).match[1];
+  const bookmarks = await listDb.getAllBookmarks(userId);
 
   await ctx.editMessageText("Refreshing...");
 
@@ -44,20 +48,41 @@ export const RefreshBookmarksAction = async (
   }
 
   const refreshedBookmarks = await listDb.getBookmarks(userId);
-  await ctx.editMessageText(getResponseStringBookmark(refreshedBookmarks), {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          { text: "Add ➕", callback_data: COMMANDS.ADD },
-          { text: "Remove ❌", callback_data: COMMANDS.REMOVE },
-        ],
-        [{ text: "Refresh 🔄", callback_data: COMMANDS.REFRESH }],
-      ],
-    },
-    link_preview_options: {
-      is_disabled: true,
-    },
-  });
 
-  ctx.answerCbQuery();
+  if (refreshedBookmarks && refreshedBookmarks.length > 0) {
+    const totalBookmarkCount =
+      (await listDb.getTotalBookmarkCount(userId)) || 0;
+    const hasMore = totalBookmarkCount > (+pageNumber + 1) * PAGE_SIZE;
+
+    let command = [...DEFAULT_GET_INLINE_KEYBOARD_COMMANDS];
+    const hasPrevPage = +pageNumber > 0;
+    const pageCommand = [];
+    if (hasPrevPage)
+      pageCommand.push({
+        text: "Previous ⬅️",
+        callback_data: `${COMMANDS.LIST}:${+pageNumber - 1}`,
+      });
+
+    if (hasMore) {
+      pageCommand.push({
+        text: "Next ➡️",
+        callback_data: `${COMMANDS.LIST}:${+pageNumber + 1}`,
+      });
+    }
+
+    if (pageCommand.length > 0) {
+      command = [pageCommand, ...command];
+    }
+
+    await ctx.editMessageText(getResponseStringBookmark(refreshedBookmarks), {
+      reply_markup: {
+        inline_keyboard: command,
+      },
+      link_preview_options: {
+        is_disabled: true,
+      },
+    });
+
+    ctx.answerCbQuery();
+  }
 };
