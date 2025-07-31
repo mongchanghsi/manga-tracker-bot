6547f5617f2c005782ef537113397948e8c394d4
Eric Mong <m.changhsi@gmail.com>
2025-07-31 09:38:32 +0800

    feat: update message

--- utils/messages.ts
+++ utils/messages.ts
@@ -5,7 +5,7 @@ export const BOOKMARK_NONE =
   "You don't have any bookmarked manga! Add one to get started";
 export const BOOKMARK_ADD_RESPONSE_1 = "What is the name?";
 export const BOOKMARK_ADD_RESPONSE_2 =
-  "What is the URL of the latest chapter (Please make sure the URL contains the latest chapter number. Recommended kunmanga/mangafire/kingofshojo/manhuaus)";
+  "What is the URL of the latest chapter (Please make sure the URL contains the latest chapter number. Recommended mangafire/kingofshojo/manhuaus)";
 export const BOOKMARK_ADD_RESPONSE_3 = "What is latest chapter for this URL";
 export const BOOKMARK_ADD_SUCCESS = "Added to your bookmarks!";
 
@@ -27,4 +27,4 @@ export const COMPLETED_NONE =
   "You don't have any completed manga! Add one and share your list to your friends";
 
 export const SERVICE_PAUSED = `We are currently in maintance. We will provide an announcement when the service is up!`;
-export const OWNER_MAINTANENCE_MESSAGE = `Hello! 👋\nI'm the owner of the Manga Tracker bot. I noticed that some of the fields during your manga registration were entered incorrectly, so I’ve gone ahead and helped update the information for you. 😊\n\nIf the manga that was added isn't what you intended, feel free to remove it anytime using the /remove command along with the manga ID.\n\n————————\n\n📌 How to properly register a manga:\nUse the /add command\n\nEnter the title of the manga\ne.g. One Piece\n\nPaste the URL of the latest chapter\ne.g. https://comick.io/comic/01-one-piece/O5E6cA7u-chapter-1145-en\n\nEnter the latest chapter number\ne.g. 1145 — this should match the chapter number in the URL\n\n✅ You're done — the manga is now being tracked!\n\n————————\n\n🔍 Need help finding a source?\nTry using sites like comick.io or kunmanga.com\n\n————————\n\n🛠️ This bot is still in development!\nIf you have any feedback or suggestions, feel free to use the /feedback command — I'd love to hear from you!`;
+export const OWNER_MAINTANENCE_MESSAGE = `Hello! 👋\nI'm the owner of the Manga Tracker bot. I noticed that some of the fields during your manga registration were entered incorrectly, so I’ve gone ahead and helped update the information for you. 😊\n\nIf the manga that was added isn't what you intended, feel free to remove it anytime using the /remove command along with the manga ID.\n\n————————\n\n📌 How to properly register a manga:\nUse the /add command\n\nEnter the title of the manga\ne.g. One Piece\n\nPaste the URL of the latest chapter\ne.g. https://comick.io/comic/01-one-piece/O5E6cA7u-chapter-1145-en\n\nEnter the latest chapter number\ne.g. 1145 — this should match the chapter number in the URL\n\n✅ You're done — the manga is now being tracked!\n\n————————\n\n🔍 Need help finding a source?\nTry using sites like comick.io\n\n————————\n\n🛠️ This bot is still in development!\nIf you have any feedback or suggestions, feel free to use the /feedback command — I'd love to hear from you!`;
