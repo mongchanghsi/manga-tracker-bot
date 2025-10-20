5f801f58df09973e1186f83465066454d7a1a4b6
Eric Mong <m.changhsi@gmail.com>
2025-10-21 00:59:29 +0800

    disable xbato source

--- modules/Bookmark/add.ts
+++ modules/Bookmark/add.ts
@@ -76,12 +76,9 @@ export const AddBookmarksFollowup = async (
           ],
           [
             { text: SOURCE.HARIMANGA, callback_data: SOURCE.HARIMANGA },
-            { text: SOURCE.XBATO, callback_data: SOURCE.XBATO },
-          ],
-          [
             { text: SOURCE.COMICK_LIVE, callback_data: SOURCE.COMICK_LIVE },
-            { text: SOURCE.OTHERS, callback_data: SOURCE.OTHERS },
           ],
+          [{ text: SOURCE.OTHERS, callback_data: SOURCE.OTHERS }],
         ],
       },
     });
